62d3fc6eccef928597e4f20fdb543a7240fd9998
Victor Martinez <vicmargar@gmail.com>
2014-11-19 22:12:21 +0000

    load items from JSON file

--- app/src/main/java/com/example/victor/materialtest/BackgroundList.java
+++ app/src/main/java/com/example/victor/materialtest/BackgroundList.java
@@ -2,20 +2,45 @@ package com.example.victor.materialtest;
 
 import android.app.Activity;
 import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BackgroundList extends AsyncTask<Void, Void, ArrayList> {
-    public DisplayableList activity;
 
+    public DisplayableList activity;
     public BackgroundList(DisplayableList a) {
         activity = a;
     }
 
+    private ArrayList<String> loadFromJSON() {
+        String json = AssetUtil.loadJSON((Activity) activity, "items.json");
+        ArrayList<String> list = new ArrayList<String>();
+        try{
+            JSONObject data = new JSONObject(json);
+            JSONArray items = new JSONArray(data.getString("data"));
+
+            int n = items.length();
+            for (int i = 0; i < n; i++) {
+                JSONObject jo = items.getJSONObject(i);
+                String name = jo.getString("name");
+                list.add(name);
+            }
+        }
+        catch(JSONException je) {
+            System.out.println( "Error: " + je.toString());
+        }
+        return list;
+    }
+
     protected ArrayList<String> doInBackground(Void... params) {
-        ArrayList<String> items = new ArrayList<String>(
-            Arrays.asList("Android", "iPhone", "WindowsMobile", "Blackberry", "WebOS", "Ubuntu", "Windows7", "Mac OS X")
-        );
+        ArrayList <String> items = loadFromJSON();
 
         try {
             Thread.sleep(500);
